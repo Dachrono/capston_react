6eb6158781a0a0e63363daf30cbc5da97c09bf16
Dachrono <andres_zamorano_@hotmail.com>
2023-07-30 14:20:36 -0600

    Create store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />;
+  </Provider>
 );
