338f6abe08777c17ba45d9c852762a0c6c31de8f
Dachrono <andres_zamorano_@hotmail.com>
2023-08-05 01:47:03 -0600

    Add assets to use in the project

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -15,3 +15,19 @@ export function Cog() {
     </svg>
   );
 }
+
+export function Arrow() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+  );
+}
+
+export function RevArrow() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+    </svg>
+  );
+}
